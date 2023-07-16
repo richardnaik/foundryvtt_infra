310bd6095a770c5f5df41876bdcf9a60e8fcb82d
Richard Naik <richardnaik@gmail.com>
2023-07-15 20:30:46 -0500

    adding providers file

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-provider "google" {
-  project = var.project
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
-# resource "google_service_account" "default" {
-#   account_id   = "service_account_id"
-#   display_name = "Service Account"
-# }
-
 resource "google_compute_instance" "foundry" {
   name         = "foundry"
   machine_type = "e2-medium"
@@ -19,7 +8,6 @@ resource "google_compute_instance" "foundry" {
             size  = 60
         }
     }
-  
 
   network_interface {
     network = "default"
---
+++ providers.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = var.project
+  region  = "us-central1"
+  zone    = "us-central1-f"
+}
